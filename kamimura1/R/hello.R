fc1ab11cd22836ecc866b43c0831561a87dea076
dsbiztiu22150153 <166496593+tokikami@users.noreply.github.com>
2024-07-01 17:58:31 +0900

    Add files via upload

---
+++ kamimura1/R/hello.R
@@ -0,0 +1,21 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Konchiwa, world!")
+  d <- data.frame(x=1:3,y=1:3)
+  ggplot2::ggplot(d,mapping==ggplot2::aes(x=1,y=2))+ggplot2::geom_point()
+}
+
